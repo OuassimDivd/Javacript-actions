0bf0074a4b7b142cd572f6232d69e08467d440ad
OuassimDivd <115215763+OuassimDivd@users.noreply.github.com>
2022-10-07 13:45:33 +0200

    Update main.js

--- mkdir -p .github/actions/.github/actions/joke-action/main.js
+++ mkdir -p .github/actions/.github/actions/joke-action/main.js
@@ -8,3 +8,14 @@ async function run() {
 }
 
 run();
+
+const getJoke = require("./joke");
+const core = require("@actions/core");
+
+async function run() {
+  const joke = await getJoke();
+  console.log(joke);
+  core.setOutput("joke-output", joke);
+}
+
+run();
